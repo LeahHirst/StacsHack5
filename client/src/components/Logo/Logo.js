c8cd8cc1ca78ea98158a2f26065dddb26955b6c0
Leah Hirst <leah@hirst.dev>
2019-03-02 14:38:19 +0000

    Added Logo component

--- client/src/components/Logo/Logo.js
+++ client/src/components/Logo/Logo.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
+
+import LogoRaw from './Logo.svg';
 
 export default class Logo extends React.Component {
     render() {
-        
+        return (
+            <LogoDiv>
+                <img src={LogoRaw} />
+            </LogoDiv>
+        )
     }
 }
+
+const LogoDiv = styled.div`
+
+`;
