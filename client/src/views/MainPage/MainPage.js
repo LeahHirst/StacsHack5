adc67baab14766d7ff4efa15669690ecf84cc127
Ben Banerjee-Richards <contact@benbr.com>
2019-03-02 15:22:37 +0000

    API Request

--- client/src/views/MainPage/MainPage.js
+++ client/src/views/MainPage/MainPage.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 export default class MainPage extends React.Component {
     render() {
-        
+        return(
+            <h1>Hello world!</h1>
+        )
     }
 }
