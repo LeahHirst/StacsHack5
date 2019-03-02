d9fa25b6e42308d4621a3f3769b45900f866c022
Leah Hirst <leah@hirst.dev>
2019-03-02 14:45:06 +0000

    Added header component

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Logo } from './components';
+import { Logo, Header } from './components';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <Logo></Logo>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Header />
     );
   }
 }
