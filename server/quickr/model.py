53dbbbaa8d542326b017f73577c3bd8f48789a8f
Jacob-Mowat <jacob@jmowat.me>
2019-03-02 19:46:22 +0000

    Inital tensorflow setup.

--- server/quickr/model.py
+++ server/quickr/model.py
@@ -1,2 +1,29 @@
+import tensorflow as tf
 
-# TODO: load tflite model
+def run_on_model(model_path: str, message_data: str):
+    interpreter = tf.lite.Interpreter(model_path=model_path)
+    interpreter.allocate_tensors()
+
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+
+    input_shape = input_details[0]['shape']
+
+    input_data = message_data.split(' ')
+
+    interpreter.set_tensor(input_details[0]['index'], input_data)
+
+    interpreter.invoke()
+
+    output_data = interpreter.get_tensor(output_details[0]['index'])
+
+    print(output_data)
+
+
+def main():
+    run_on_model("../../model/smartreply.tflite", "Hello how are you doing?")
+
+if __name__ == "__main__":
+    main()
+
+
