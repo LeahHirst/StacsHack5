30c7e55664bcb3e3579d00133cf145be02380b5a
samwalls <sam@s-w.io>
2019-03-03 11:04:28 +0000

    asd

--- server/cbindings/main.cc
+++ server/cbindings/main.cc
@@ -1,22 +1,23 @@
 #include "tf_wrapper.h"
 #include <iostream>
 
-int main() {
+int main(int argc, char** argv) {
+
+  const char* fileDir = argv[1];
+
+  // TODO:
   const char* w_1 = "okay";
   const char* w_2 = "yes";
   const char* w_3 = "null";
   const char* words[] = { w_1, w_2, w_3 };
 
-  ModelStorage* modelPtr = reinterpret_cast<ModelStorage*>(loadModel("/Users/jacob/Documents/Stacshax5/StacsHack5/model/smartreply.tflite", words, 3));
 
-  const char* in_1 = "How";
-  const char* in_2 = "are";
-  const char* in_3 = "you?";
-  const char* input[] = { in_1, in_2, in_3 };
+  // input + 2 to get all the rest of the arguments
+  ModelStorage* modelPtr = reinterpret_cast<ModelStorage*>(loadModel(fileDir, words, 3));
 
-  PredictionOutput* output = reinterpret_cast<PredictionOutput*>(predict(modelPtr, input, 3));
+  PredictionOutput* output = reinterpret_cast<PredictionOutput*>(predict(modelPtr, const_cast<const char**>(argv) + 2, argc - 1));
 
   for (int i = 0; i < output->results_length; i++) {
-    std::cout << output->results[i]->text << " => " << output->results[i]->score << std::endl;
+    std::cout << output->results[i]->text << ", " << output->results[i]->score << std::endl;
   }
 }
