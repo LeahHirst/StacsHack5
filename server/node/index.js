b3c67d989f16f59d6a7ee128934383b6284355b1
Adam Hirst <adam@ahirst.com>
2019-03-03 11:22:49 +0000

    Fixed endpoint

--- server/node/index.js
+++ server/node/index.js
@@ -3,9 +3,10 @@ const app = express();
 const port = 3000;
 const { spawn } = require('child_process');
 const modelPath = '../../model/smartreply.tflite';
+const script = '/Users/adamhirst/tensorflow/bazel-bin/tensorflow/lite/models/smartreply/demo/app/src/main/test';
 
 app.get('/api', (req, res) => {
-    const message = req.params.message;
+    const message = req.query.message;
     predict(message, (success) => {
         res.send(success);
     }, (err) => {
@@ -15,14 +16,17 @@ app.get('/api', (req, res) => {
 
 
 function predict(message, callback, err) {
-    const child = spawn('echo', [modelPath, message]);
+    console.log([modelPath, message]);
+    const child = spawn(script, [modelPath, message]);
     child.stdout.on('data', (data) => {
+        data = `${data}`;
         if (data === undefined || data === '' || data.split === undefined) { err('An error occurred'); return; }
         let lines = data.split('\n');
-        lines.map(line => {
-            let values = line.split(',');
+        lines = lines.map(line => {
+            let values = line.split(', ');
             return { text: values[0], score: values[1] };
         });
+        lines = lines.slice(0, lines.length - 1);
         callback(lines);
     });
     child.stdout.on('error', err);
